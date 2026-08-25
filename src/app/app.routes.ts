b206567c91dfd8b33b8213a9be8f0ddb6a7ca9f9
Thomas Dumez <dumezthomas@gmail.com>
2026-08-25 23:36:24 +0200

    feat: add home page and application routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,46 @@
 import {Routes} from '@angular/router';
+
+import {authGuard} from './core/guards/auth.guard';
+import {guestGuard} from './core/guards/guest.guard';
+
+import {GuestLayout} from './core/layout/guest-layout/guest-layout';
+import {MainLayout} from './core/layout/main-layout/main-layout';
+
 import {Login} from './features/auth/pages/login/login';
 import {Dashboard} from './features/dashboard/pages/dashboard/dashboard';
-import {authGuard} from './core/guards/auth.guard';
+import {Home} from './features/home/pages/home/home';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: GuestLayout,
+    canActivate: [guestGuard],
+    children: [
+      {
+        path: '',
+        component: Home
+      },
+
+      {
+        path: 'login',
+        component: Login
+      }
+    ]
+  },
 
   {
     path: '',
-    component: Login
+    component: MainLayout,
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'dashboard',
+        component: Dashboard
+      }
+    ]
   },
-
-  {
-    path: 'dashboard',
-    component: Dashboard,
-    canActivate: [authGuard]
-  },
-
   {
     path: '**',
     redirectTo: ''
   }
-
 ];
--- src/app/features/home/pages/home/home.ts
+++ src/app/features/home/pages/home/home.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {RouterLink} from '@angular/router';
 
 @Component({
-  imports: [],
   selector: 'app-home',
-  styleUrl: './home.scss',
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    RouterLink
+  ],
   templateUrl: './home.html',
+  styleUrl: './home.scss'
 })
-export class Home {}
+export class Home {
+}
